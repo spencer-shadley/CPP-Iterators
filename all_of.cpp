d8115e243187e6e52a7bbfb59fab709015e4d5c4
U-Spencer-MSI\Spencer <spencershadley@utexas.edu>
2015-10-04 18:51:29 -0500

    added all_of

---
+++ all_of.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <assert.h>
+using namespace std;
+
+namespace spencer {
+	template < typename II, typename FN >
+	bool all_of(II b, II e, FN fn) {
+		while (b != e) {
+			if (!fn(*b)) return false;
+			b++;
+		}
+		return true;
+	}
+}
+
+void main() {
+	vector<int> x = { 1, 2, 3 };
+	assert(spencer::all_of(x.begin(), x.end(),
+		[] (int x) -> bool { return x < 4; }));
+	x[2] = 4;
+	assert(!spencer::all_of(x.begin(), x.end(),
+		[] (int x) -> bool { return x < 4; }));
+	assert(!spencer::all_of(x.begin(), x.end(),
+		[] (int x) -> bool { return x == 1 || x == 4 || x == 3; }));
+}
