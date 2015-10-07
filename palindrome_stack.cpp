038d887c53b173d6bdca8a864a7051c975752f97
U-Spencer-MSI\Spencer <spencershadley@utexas.edu>
2015-10-06 21:13:16 -0500

    stack fixes

--- palindrome_stack.cpp
+++ palindrome_stack.cpp
@@ -12,26 +12,28 @@ public:
 	stack() : v() {}
 	stack(vector<T> v_given) : v(v_given) {}
 
-	// push
 	void push(const T& value) {
 		v.push_back(value);
 	}
 
-	// pop
-	T pop() {
+	T& pop() {
 		assert(v.size() > 0);
+		T popped = v[v.size()-1];
 		v.pop_back();
+		return popped;
 	}
 
-	// peek
 	const T& peek() const {
 		assert(v.size() > 0);
 		return v[v.size() - 1];
 	}
 
-	// size
 	int size() {
 		return v.size();
+	}
+
+	T& operator[](int index) {
+		return v[index];
 	}
 };
 
@@ -54,6 +56,13 @@ void main() {
 	string s = "racessqwercar";
 	cout << is_palindrome(s) << endl;
 
-
-
+	stack<int> st = stack<int>();
+	st.push(3);
+	int temp = 4;
+	st.push(temp);
+	assert(st.peek() == 4);
+	assert(st.pop() == 4);
+	assert(st.pop() == 3);
+	st.push(42);
+	assert(st.pop() == 42);
 }
